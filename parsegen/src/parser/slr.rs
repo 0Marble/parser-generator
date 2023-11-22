93ccbca356a55b3429d557d424209fd670f3602e
Aleksei Lobanov <zeromarblectm@gmail.com>
2023-11-22 14:46:38 +0800

    lr0 automata

--- parsegen/src/parser/slr.rs
+++ parsegen/src/parser/slr.rs
@@ -1,9 +1,6 @@
-use crate::{parser::grammar::Production, regex::state_machine::Nfa, tokenizer::Token};
+use crate::{parser::grammar::Production, tokenizer::Token};
 
-use super::{
-    grammar::{Follow, Grammar},
-    lgraph::Lgraph,
-};
+use super::{grammar::Grammar, lgraph::Lgraph};
 
 impl Lgraph {
     pub fn slr(grammar: &Grammar) -> Self {
@@ -20,28 +17,86 @@ impl Lgraph {
         );
         let first = grammar.first();
         let follow = grammar.follow(&first);
+        let (lr0_states, lr0_goto) = Self::lr0_automata(&grammar);
+
+        todo!()
+    }
+
+    fn lr0_automata(grammar: &Grammar) -> (Vec<Vec<(usize, usize)>>, Vec<(usize, Token, usize)>) {
         let (start_index, _) = grammar
             .productions()
             .enumerate()
-            .find(|(_, s)| s.lhs() == new_start)
+            .find(|(_, s)| s.lhs() == grammar.start())
             .unwrap();
 
-        let mut stack = vec![vec![(0, 0)]];
-        let mut lr0_states: Vec<Vec<(i32, i32)>> = vec![];
+        let mut stack = vec![vec![(start_index, 0)]];
+        let mut lr0_states: Vec<Vec<(usize, usize)>> = vec![];
         let mut lr0_goto = vec![];
         while let Some(state) = stack.pop() {
-            let state_index = if let Some((i, _)) = lr0_states
-                .iter()
-                .enumerate()
-                .find(|(_, s)| s.as_slice() == state.as_slice())
-            {
-                i
-            } else {
-                lr0_states.push(state.clone());
-                lr0_states.len() - 1
-            };
+            let state_index = lr0_states.len();
+            lr0_states.push(state);
+            let state = lr0_states[state_index].as_slice();
+
+            let mut full_state = state.to_vec();
+            for (prod_index, spot) in state {
+                let symbol = grammar
+                    .productions()
+                    .nth(*prod_index)
+                    .unwrap()
+                    .rhs()
+                    .get(*spot)
+                    .unwrap();
+                if grammar.is_terminal(symbol.clone()) {
+                    continue;
+                }
+
+                for (other_idx, prod) in grammar.productions().enumerate() {
+                    if &prod.lhs() != symbol || full_state.contains(&(other_idx, 0)) {
+                        continue;
+                    }
+                    full_state.push((other_idx, 0));
+                }
+            }
+
+            let mut goto: Vec<(&Token, Vec<(usize, usize)>)> = vec![];
+            for (prod_index, spot) in full_state {
+                let prod = grammar.productions().nth(prod_index).unwrap();
+                if prod.rhs().len() <= spot + 1 {
+                    continue;
+                }
+                let symbol = prod.rhs().get(spot).unwrap();
+
+                let goto_on_symbol =
+                    if let Some((_, g)) = goto.iter_mut().find(|(s, _)| s == &symbol) {
+                        g
+                    } else {
+                        goto.push((symbol, vec![]));
+                        &mut goto.last_mut().unwrap().1
+                    };
+                if !goto_on_symbol.contains(&(prod_index, spot + 1)) {
+                    goto_on_symbol.push((prod_index, spot + 1));
+                }
+            }
+
+            for (symbol, next_state) in goto {
+                let next_state_index = if let Some((i, _)) = lr0_states
+                    .iter()
+                    .enumerate()
+                    .find(|(_, s)| s.as_slice() == next_state.as_slice())
+                {
+                    i
+                } else {
+                    stack.push(next_state);
+                    lr0_states.len()
+                };
+
+                let edge = (state_index, symbol.clone(), next_state_index);
+                if !lr0_goto.contains(&edge) {
+                    lr0_goto.push(edge);
+                }
+            }
         }
 
-        todo!()
+        (lr0_states, lr0_goto)
     }
 }
